f2d40bb3a1e8279454d823bb4ee7569b374279cf
Bleuitup <46002006+Bleuitup@users.noreply.github.com>
2026-04-28 18:09:24 -0400

    Add files via upload
    Update for version 1.2

--- Blast to The Past/output/lua/B2TP/GUI/Post/GUIFeedback.lua
+++ Blast to The Past/output/lua/B2TP/GUI/Post/GUIFeedback.lua
@@ -3,7 +3,7 @@ function GUIFeedback:Initialize()
     oldInitialize(self)
 
     local oldText = self.buildText:GetText()
-    local newText = oldText .. " - B2TP v1.1" 
+    local newText = oldText .. " - B2TP v1.2" 
     
     self.buildText:SetText(newText)
 end
