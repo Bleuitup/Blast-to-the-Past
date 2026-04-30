79049b2c2a1de8972913d635b06fba64893698ab
Bleuitup <46002006+Bleuitup@users.noreply.github.com>
2026-04-30 18:31:45 -0400

    Add files via upload
    Compmod cyst damage and shift hive scalar changes

--- Blast to The Past/output/lua/B2TP/Cysts/Post/Cyst.lua
+++ Blast to The Past/output/lua/B2TP/Cysts/Post/Cyst.lua
@@ -1,10 +1,15 @@
-function Cyst:GetIsFlameableMultiplier()
-    return kCystFlameableMultiplier
+function Cyst:GetIsFlameableMultiplier(doer)
+    if doer and doer.isa and doer:isa("Welder") then
+        return 4
+    else
+        return 7
+    end
 end
 
 function Cyst:GetAutoBuildRateMultiplier()
     if GetHasTech(self, kTechId.ShiftHive) then
-        return kCystBuildShiftMultiplier
+        -- return 1.25
+        return 1.5 -- increase by 20% from vanilla
     end
 
     return 1
