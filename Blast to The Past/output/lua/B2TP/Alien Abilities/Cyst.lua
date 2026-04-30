0d6a62bf5160e4faf8e6df7361cdcc05fb319381
Bleuitup <46002006+Bleuitup@users.noreply.github.com>
2026-04-30 16:53:28 -0400

    Add files via upload
    Keep welders at x4 flame damage but buff clusters and flamers (x7)

---
+++ Blast to The Past/output/lua/B2TP/Alien Abilities/Cyst.lua
@@ -0,0 +1,16 @@
+function Cyst:GetIsFlameableMultiplier(doer)
+    if doer and doer.isa and doer:isa("Welder") then
+        return 4
+    else
+        return 7
+    end
+end
+
+function Cyst:GetAutoBuildRateMultiplier()
+    if GetHasTech(self, kTechId.ShiftHive) then
+        -- return 1.25
+        return 1.5 -- increase by 20% from vanilla
+    end
+
+    return 1
+end
