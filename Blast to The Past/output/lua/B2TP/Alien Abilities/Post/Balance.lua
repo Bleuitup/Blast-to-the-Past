afad6c2992221ddff99168f544aedb94fb4d0632
Bleuitup <46002006+Bleuitup@users.noreply.github.com>
2026-05-01 02:58:38 -0400

    Add files via upload
    Advanced Swipe constants

--- Blast to The Past/output/lua/B2TP/Alien Abilities/Post/Balance.lua
+++ Blast to The Past/output/lua/B2TP/Alien Abilities/Post/Balance.lua
@@ -3,3 +3,11 @@ kLeapEnergyCost = 55 -- was 45
 
 --Lerk
 kSpikeDamage = 5.5 --was 5
+
+--CBM Stomp
+kStompDamage = 50 --was 40
+
+--Adv Swipe
+kAdvancedSwipeDamageScalar = 1.08
+kAdvancedSwipeResearchTime = 60
+kAdvancedSwipeCost = 25
