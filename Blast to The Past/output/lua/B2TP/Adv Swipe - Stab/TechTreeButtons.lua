aec619a1edeade3577e6a143e8663eeb4a06bf26
Bleuitup <46002006+Bleuitup@users.noreply.github.com>
2026-05-01 15:22:20 -0400

    Delete Blast to The Past/output/lua/B2TP/Adv Swipe - Stab/TechTreeButtons.lua

--- Blast to The Past/output/lua/B2TP/Adv Swipe - Stab/TechTreeButtons.lua
+++
@@ -1,6 +0,0 @@
-local kTechIdToMaterialOffset = debug.getupvaluex(GetMaterialXYOffset, "kTechIdToMaterialOffset")
-
-if kTechIdToMaterialOffset then
-    kTechIdToMaterialOffset[kTechId.AdvancedSwipe] = 105
-    kTechIdToMaterialOffset[kTechId.Stab] = 194
-end
