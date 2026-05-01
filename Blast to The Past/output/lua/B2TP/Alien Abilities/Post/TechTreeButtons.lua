46b7bc9c43b193d2a6bd308acd9dfe47ed41ffd7
Bleuitup <46002006+Bleuitup@users.noreply.github.com>
2026-05-01 15:06:55 -0400

    Delete Blast to The Past/output/lua/B2TP/Alien Abilities/Post/TechTreeButtons.lua

--- Blast to The Past/output/lua/B2TP/Alien Abilities/Post/TechTreeButtons.lua
+++
@@ -1,7 +0,0 @@
-local kTechIdToMaterialOffset = debug.getupvaluex(GetMaterialXYOffset, "kTechIdToMaterialOffset")
-
-if kTechIdToMaterialOffset and not kTechIdToMaterialOffset[kTechId.AdvancedSwipe] then
-
-    kTechIdToMaterialOffset[kTechId.AdvancedSwipe] = 105
-	
-end
