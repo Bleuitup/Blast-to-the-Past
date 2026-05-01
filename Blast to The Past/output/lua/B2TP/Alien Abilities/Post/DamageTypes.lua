28ed669940449a774869238fa330aaa3eb54e694
Bleuitup <46002006+Bleuitup@users.noreply.github.com>
2026-05-01 13:36:56 -0400

    Refactor NS2Gamerules_GetUpgradedDamage function
    Correction to do away with the old compmod logic and check for the research of the correct tech.

--- Blast to The Past/output/lua/B2TP/Alien Abilities/Post/DamageTypes.lua
+++ Blast to The Past/output/lua/B2TP/Alien Abilities/Post/DamageTypes.lua
@@ -1,8 +1,14 @@
 local oldGetUpgradedDamage = NS2Gamerules_GetUpgradedDamage
+
 function NS2Gamerules_GetUpgradedDamage(attacker, doer, damage, damageType, hitPoint)
-    if doer.kMapName == "swipe" and attacker:GetHasThreeHives() then
+
+    if doer
+    and doer.kMapName == "swipe"
+    and attacker
+    and GetHasTech(attacker, kTechId.AdvancedSwipe) then
         return damage * kAdvancedSwipeDamageScalar
     end
 
     return oldGetUpgradedDamage(attacker, doer, damage, damageType, hitPoint)
+
 end
