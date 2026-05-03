4fd87058d11e2a64e8e290f318a2ae07608622e9
Bleuitup <46002006+Bleuitup@users.noreply.github.com>
2026-05-02 20:02:52 -0400

    Update version text from v1.21 to v1.22

--- Blast to The Past/output/lua/B2TP/GUI/Post/GUIFeedback.lua
+++ Blast to The Past/output/lua/B2TP/GUI/Post/GUIFeedback.lua
@@ -3,7 +3,7 @@ function GUIFeedback:Initialize()
     oldInitialize(self)
 
     local oldText = self.buildText:GetText()
-    local newText = oldText .. " - B2TP v1.21" 
+    local newText = oldText .. " - B2TP v1.22" 
     
     self.buildText:SetText(newText)
 end
